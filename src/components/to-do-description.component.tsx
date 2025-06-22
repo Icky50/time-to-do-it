44c7f587f0d4e156b2f056b54127a5703bf1e38a
Hannes Knauss <hc.knauss@web.de>
2025-06-22 18:05:19 +0200

    added modeling to work with timers

--- src/components/to-do-description.component.tsx
+++ src/components/to-do-description.component.tsx
@@ -4,20 +4,24 @@ interface ToDoDescriptionProps {
     onChange?: (description: string) => void;
 }
 
-export function ToDoDescription({ description, editMode, onChange }: ToDoDescriptionProps) {
+export function ToDoDescription({
+    description,
+    editMode,
+    onChange,
+}: ToDoDescriptionProps) {
     return (
         <textarea
             defaultValue={description || "No description"}
             className="
-							bg-blue-500
-                            border-t-3 border-gray-400
-							rounded-b-md
-							px-3 py-2
-							min-h-[70px]
-							w-full
-							resize-none
-                            focus:outline-none
-						"
+				bg-blue-500
+                border-t-3 border-gray-400
+				rounded-b-md
+				px-3 py-2
+				min-h-[70px]
+				w-full
+				resize-none
+                focus:outline-none
+				"
             rows={3}
             readOnly={!editMode}
             disabled={!editMode}
--- src/models/to-do-element.model.ts
+++ src/models/to-do-element.model.ts
@@ -3,11 +3,15 @@ export class ToDoElement {
     name: string;
     description: string;
     time: number;
+    isTimerRunning: boolean;
+    lastTimerStart: number | null;
 
-    constructor(id: string, name: string, description: string, time: number) {
+    constructor(id: string, name: string, description: string, time: number, isTimerRunning: boolean = false, lastTimerStart: number | null = null) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.time = time;
+        this.isTimerRunning = isTimerRunning;
+        this.lastTimerStart = lastTimerStart;
     }
 }
